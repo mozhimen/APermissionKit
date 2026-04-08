a186ad9bd5fdfd8a294cb644132e913e2f2a1734
mozhimen <58215586+mozhimen@users.noreply.github.com>
2026-04-08 17:03:13 +0800

    260408

--- permissionk/test/src/main/java/com/mozhimen/permissionk/test/PermissionKActivity.kt
+++ permissionk/test/src/main/java/com/mozhimen/permissionk/test/PermissionKActivity.kt
@@ -1,14 +1,13 @@
 package com.mozhimen.permissionk.test
 
 import android.view.View
-import com.mozhimen.bindk.bases.viewdatabinding.activity.BaseActivityVDB
 import com.mozhimen.kotlin.elemk.android.cons.CPermission
-import com.mozhimen.manifestk.permission.ManifestKPermission
-import com.mozhimen.manifestk.permission.annors.APermissionCheck
 import com.mozhimen.kotlin.utilk.android.app.UtilKActivityStart
 import com.mozhimen.kotlin.utilk.android.widget.showToast
 import com.mozhimen.permissionk.PermissionK
+import com.mozhimen.permissionk.annors.APermissionCheck
 import com.mozhimen.permissionk.test.databinding.ActivityPermissionkBinding
+import com.mozhimen.uik.databinding.bases.viewdatabinding.activity.BaseActivityVDB
 
 @APermissionCheck(CPermission.WRITE_EXTERNAL_STORAGE, CPermission.READ_EXTERNAL_STORAGE)
 class PermissionKActivity : BaseActivityVDB<ActivityPermissionkBinding>() {
@@ -18,28 +17,28 @@ class PermissionKActivity : BaseActivityVDB<ActivityPermissionkBinding>() {
             if (it) {
                 "权限申请成功".showToast()
             } else {
-                UtilKActivityStart.startSettingApplicationDetailsSettings(this)
+                UtilKActivityStart.startSettingsApplicationDetailsSettings(this)
             }
         }
     }
 
     fun requestPermission2(view: View) {
         //方法二,need APermissionCheck 注解
-        ManifestKPermission.requestPermissions(this,
+        PermissionK.requestPermissions(this,
             onSuccess = {
                 "权限申请成功".showToast()
             }, onFail = {
-                UtilKActivityStart.startSettingApplicationDetailsSettings(this)
+                UtilKActivityStart.startSettingsApplicationDetailsSettings(this)
             })
     }
 
     fun requestPermission3(view: View) {
         //方法三
-        ManifestKPermission.requestPermissions(this, arrayOf(CPermission.WRITE_EXTERNAL_STORAGE, CPermission.READ_EXTERNAL_STORAGE)) {
+        PermissionK.requestPermissions(this, arrayOf(CPermission.WRITE_EXTERNAL_STORAGE, CPermission.READ_EXTERNAL_STORAGE)) {
             if (it) {
                 "权限申请成功".showToast()
             } else {
-                UtilKActivityStart.startSettingApplicationDetailsSettings(this)
+                UtilKActivityStart.startSettingsApplicationDetailsSettings(this)
             }
         }
     }
